e2672023821b033ff9a9af5ecc7b97760be8c740
gayathirithavamani <gayat2000@gmail.com>
2023-04-19 22:25:22 +0530

    schema

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
+const cors = require("cors");
 
 const app = express();
 dotenv.config();
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("API is good good");
